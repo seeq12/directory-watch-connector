bd3fa55ed55bc8830617668ebaebd085f9300e15
Winner Bolorunduro <107595751+bolorundurowb-sq@users.noreply.github.com>
2024-06-17 06:40:07 -0600

    CRAB-42921: switch to package references (#3)
    * CRAB-42921: move the connector from package config to package reference
    
    * CRAB-42921: move the timestamp tag reader from package config to package reference
    
    * CRAB-42921: move the timestamp asset tag reader from package config to package reference
    
    * CRAB-42921: move the tags with metadata reader from package config to package reference
    
    * CRAB-42921: move the offset tags reader from package config to package reference
    
    * CRAB-42921: move the narrow file reader from package config to package reference
    
    * CRAB-42921: move the conditions with properties reader from package config to package reference
    
    * CRAB-42921: fix package restoration

--- build.bat
+++ build.bat
@@ -13,13 +13,7 @@ goto :EOF
 
 if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
 
-.\nuget install Seeq.Link.Connector.DirectoryWatch\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\ConditionsWithPropertiesReader\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\NarrowFileReader\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\OffsetTagsReader\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\TagsWithMetadataReader\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\TimestampAssetTagsCsvReaderV1\packages.config -o packages
-.\nuget install DirectoryWatchFileReaders\TimestampTagsCsvReader\packages.config -o packages
+.\nuget restore Seeq.Link.Connector.DirectoryWatch.sln
 
 for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') do set MSBUILD_PATH=%%i
 
