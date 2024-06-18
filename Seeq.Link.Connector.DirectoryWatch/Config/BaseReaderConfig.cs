98d135b0bd61f2844451ee8ee88ee5c5356de709
Winner-Timothy Bolorunduro <winner.bolorunduro@seeq.com>
2024-06-18 18:47:40 +0100

    CRAB-42920: add base reader config for shared constraints

---
+++ Seeq.Link.Connector.DirectoryWatch/Config/BaseReaderConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seeq.Link.Connector.DirectoryWatch.Config {
+
+    public abstract class BaseReaderConfig {
+        public bool DebugMode { get; set; }
+        
+        public int MaxFilesPerDirectory { get; set; }
+
+        public int MaxFileSizeInKB { get; set; }
+
+        protected BaseReaderConfig(Dictionary<string, string> readerConfiguration, bool debugMode) {
+            this.DebugMode = debugMode;
+
+            this.MaxFilesPerDirectory =
+                this.getValueOrDefault(readerConfiguration, nameof(this.MaxFilesPerDirectory), 500);
+            this.MaxFileSizeInKB =
+                this.getValueOrDefault(readerConfiguration, nameof(this.MaxFileSizeInKB), 50);
+        }
+
+        protected T getValueOrDefault<T>(Dictionary<string, string> readerConfiguration, string key, T fallbackValue) {
+            if (readerConfiguration.TryGetValue(key, out var readValue)) {
+                return (T) Convert.ChangeType(readValue, typeof(T));
+            }
+
+            return fallbackValue;
+        }
+    }
+}
